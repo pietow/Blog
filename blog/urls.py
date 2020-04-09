5a1599c3482b5c62ca8a4cc5175a074322814a1b
Piet <pprotassow@posteo.de>
2020-04-09 14:32:02 +0200

    slug adress

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('<int:pk>/', views.HomeDetail.as_view(), name='home_detail'),
+    path('<slug:slug>/', views.HomeDetail.as_view(), name='home_detail'),
     path('<slug:slug>/', views.post_detail, name='post_detail'),
 ]
